4d2dbab51db38e06311a15fe51565be637461344
12S21048 Nessy Pentasonia Pangaribuan <89531324+NessyPangaribuan@users.noreply.github.com>
2024-12-13 22:21:21 +0700

    Update streamlit.py

--- Deployment/streamlit.py
+++ Deployment/streamlit.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 # Fungsi untuk memuat model CNN
 def load_model():
-    model = tf.keras.models.load_model('Deployment/model_ulos.h5')
+    model = tf.keras.models.load_model('model_ulos.h5')
     return model
 
 # Fungsi untuk melakukan prediksi pada gambar
@@ -38,7 +38,10 @@ page = st.sidebar.radio("Pilih Fitur", ["Klasifikasi Gambar", "Panduan Pengguna"
 if page == "Klasifikasi Gambar":
     st.markdown(
         """**Tentang Aplikasi:**
-        Aplikasi ini menggunakan model Convolutional Neural Network (CNN) untuk mengklasifikasikan jenis kain ulos berdasarkan gambar yang diunggah pengguna. Model ini bertujuan untuk mendukung pelestarian budaya dan meningkatkan pemahaman tentang kain tradisional ulos.
+        
+        Aplikasi ini menggunakan model **Convolutional Neural Network (CNN)** untuk mengklasifikasikan jenis kain **Ulos** berdasarkan gambar yang diunggah pengguna. 
+        
+        Model ini bertujuan untuk mendukung pelestarian budaya dan meningkatkan pemahaman tentang kain tradisional ulos.
         """
     )
 
@@ -62,8 +65,8 @@ if page == "Klasifikasi Gambar":
                 predicted_class = class_names[np.argmax(prediction)]
                 confidence = np.max(prediction) * 100
 
-                st.write(f"Predicted Class: **{predicted_class}**")
-                st.write(f"Confidence: **{confidence:.2f}%**")
+                st.write(f"**Predicted Class:** {predicted_class}")
+                st.write(f"**Confidence:** {confidence:.2f}%")
 
                 # Tampilkan visualisasi confidence level
                 plot_confidence(prediction, class_names)
@@ -71,47 +74,60 @@ if page == "Klasifikasi Gambar":
                 # Deskripsi tambahan tentang ulos yang diprediksi
                 ulos_descriptions = {
                     "Pinuncaan": """
-                    **Desain:** Ulos ini memiliki struktur yang terdiri dari lima bagian yang ditenun secara terpisah dan kemudian disatukan. Motifnya biasanya menggunakan warna-warna cerah dengan pola geometris yang khas.
+                    **Desain:**
+                    Ulos ini memiliki struktur yang terdiri dari lima bagian yang ditenun secara terpisah dan kemudian disatukan. 
+                    Motifnya biasanya menggunakan warna-warna cerah dengan pola geometris yang khas.
+
                     **Kegunaan:**
                     - Acara Resmi: Sering digunakan dalam upacara adat dan acara resmi oleh pemimpin atau raja.
                     - Pernikahan: Dipakai oleh pengantin dan keluarga dalam perayaan pernikahan.
-                    - Marpaniaran: Dililitkan sebagai kain oleh keluarga tuan rumah pada waktu pesta besar dalam acara marpaniaran.
-                    - Simbol Kehormatan: Melambangkan status dan kehormatan bagi pemakainya dalam konteks sosial.
+                    - Marpaniaran: Digunakan saat pesta besar dalam acara marpaniaran.
+                    - Simbol Kehormatan: Melambangkan status dan kehormatan bagi pemakainya.
                     """,
                     "Ragi Hidup": """
-                    **Desain:** Ulos Ragi Hidup umumnya berbentuk panjang dan lebar, dengan pola yang sederhana namun elegan. Motifnya sering kali mencerminkan kehidupan sehari-hari masyarakat Batak.
+                    **Desain:**
+                    Ulos ini berbentuk panjang dan lebar, dengan pola sederhana namun elegan.
+
                     **Kegunaan:**
-                    - Pakaian Sehari-hari: Digunakan sebagai baju atau sarung, memberikan kenyamanan bagi pemakainya.
-                    - Simbol Kehidupan: Melambangkan kehidupan dan keberlangsungan, sering dipakai dalam acara-acara penting.
+                    - Pakaian Sehari-hari: Digunakan sebagai baju atau sarung untuk kenyamanan.
+                    - Simbol Kehidupan: Melambangkan kehidupan dan keberlangsungan.
                     """,
                     "Ragi Hotang": """
-                    **Desain:** Ulos Ragi Hotang Memiliki pola yang lebih rumit dan berwarna gelap, sering kali dihiasi dengan motif tradisional Batak.
+                    **Desain:**
+                    Memiliki pola yang rumit dan berwarna gelap, sering dihiasi motif tradisional Batak.
+
                     **Kegunaan:**
-                    - Selimut: Digunakan sebagai selimut untuk memberikan kehangatan, terutama pada malam hari atau saat cuaca dingin.
-                    - Simbol Status: Memiliki nilai simbolis, sering kali digunakan oleh keluarga kaya dalam acara tertentu.
+                    - Selimut: Digunakan untuk memberikan kehangatan.
+                    - Simbol Status: Melambangkan status sosial dalam acara tertentu.
                     """,
                     "Sadum": """
-                    **Desain:** Ulos Sadum memiliki bingkai bergaris gelap di kedua sisinya, dengan warna-warna ceria di tengahnya. Pola yang digunakan biasanya cukup beragam, mencerminkan suasana bahagia.
+                    **Desain:**
+                    Memiliki bingkai bergaris gelap di sisi dengan warna ceria di tengahnya.
+
                     **Kegunaan:**
-                    - Acara Bahagia: Dipakai dalam acara-acara sukacita seperti pesta, ulang tahun, atau perayaan lainnya.
-                    - Kenang-Kenangan: Sering dijadikan sebagai hadiah atau kenang-kenangan untuk orang terkasih.
+                    - Acara Bahagia: Digunakan dalam perayaan sukacita.
+                    - Kenang-Kenangan: Sering dijadikan hadiah untuk orang terkasih.
                     """,
                     "Sibolang": """
-                    **Desain:** Ulos Sibolang memiliki warna dominan hitam dan putih dengan pola bergaris sederhana, sering dipakai pada acara penghormatan.
+                    **Desain:**
+                    Berwarna dominan hitam dan putih dengan pola bergaris sederhana.
+
                     **Kegunaan:**
-                    - Acara Duka Cita: Dipakai oleh keluarga yang berduka dalam upacara pemakaman, melambangkan kedekatan dan penghormatan kepada yang telah meninggal.
-                    - Simbol Kesedihan: Menjadi tanda kesedihan dan dukacita bagi keluarga yang ditinggalkan.
+                    - Acara Duka Cita: Dipakai dalam upacara pemakaman untuk menghormati yang meninggal.
+                    - Simbol Kesedihan: Melambangkan duka cita.
                     """,
                     "Tumtuman": """
-                    **Desain:** Ulos Tumtuman memiliki pola geometris unik, melambangkan harapan untuk masa depan yang cerah. Ulos ini memiliki motif tali-tali yang khas, memberikan kesan kuat dan terikat.
+                    **Desain:**
+                    Memiliki pola geometris unik, melambangkan harapan untuk masa depan cerah.
+
                     **Kegunaan:**
-                    - Digunakan pada acara yang melibatkan harapan akan masa depan yang sukses dan bahagia.
-                    - Ikatan Keluarga: Dipakai oleh anak pertama dalam keluarga, simbol dari status dan tanggung jawab.
-                    - Acara Tradisional: Digunakan dalam berbagai upacara adat untuk menunjukkan posisi dalam struktur keluarga.
+                    - Acara Tradisional: Digunakan untuk menunjukkan posisi dalam keluarga.
+                    - Ikatan Keluarga: Dipakai oleh anak pertama dalam keluarga sebagai simbol tanggung jawab.
                     """
                 }
                 
-                st.markdown(f"""**Tentang {predicted_class}:**\n{ulos_descriptions.get(predicted_class, 'Deskripsi untuk jenis ulos ini belum tersedia.')}""")
+                st.markdown(f"""**Tentang {predicted_class}:**
+                {ulos_descriptions.get(predicted_class, 'Deskripsi untuk jenis ulos ini belum tersedia.')}""")
             else:
                 st.error("Model output dimensions do not match the number of class names. Please check the model and class labels.")
 
@@ -122,21 +138,17 @@ elif page == "Panduan Pengguna":
     st.header("Panduan Pengguna")
     st.markdown(
         """ 
+        ### Cara Menggunakan Aplikasi
         1. **Unggah Gambar:** Klik tombol "Browse Files" untuk mengunggah gambar ulos dalam format JPG, JPEG, atau PNG.<br>
         2. **Validasi Gambar:** Pastikan gambar yang diunggah memiliki kualitas baik dan menampilkan kain ulos dengan jelas.<br>
         3. **Hasil Prediksi:** Tunggu beberapa saat untuk melihat hasil klasifikasi dan tingkat kepercayaan model.<br>
         4. **Informasi Tambahan:** Bacalah deskripsi singkat tentang jenis ulos yang terdeteksi untuk memperkaya pengetahuan.
-        """,
-        unsafe_allow_html=True
-    )
-    
-    st.header("Informasi Tambahan tentang Kain Ulos")
-    st.markdown(
-        """
-        - **Makna Simbolis:** Kain ulos bukan hanya sekadar kain, tetapi juga memiliki nilai filosofis yang mendalam. Setiap jenis ulos memiliki makna yang melambangkan status sosial, hubungan keluarga, atau doa untuk kebahagiaan.
-        - **Proses Pembuatan:** Kain ulos dibuat menggunakan alat tenun tradisional dengan teknik yang diwariskan secara turun-temurun. Proses ini mencerminkan ketekunan dan keahlian tinggi dari para pengrajin.
-        - **Penggunaan Modern:** Saat ini, ulos tidak hanya digunakan dalam upacara adat tetapi juga sebagai inspirasi dalam desain busana modern, seperti pakaian, tas, dan aksesori.
-        - **Pelestarian Budaya:** Penggunaan ulos dalam kehidupan sehari-hari dan pengenalan kepada generasi muda menjadi bagian penting dari upaya melestarikan warisan budaya Batak.
+        
+        ### Informasi Tambahan tentang Kain Ulos
+        - **Makna Simbolis:** Kain ulos memiliki nilai filosofis yang melambangkan status sosial, hubungan keluarga, atau doa untuk kebahagiaan.
+        - **Proses Pembuatan:** Dibuat dengan alat tenun tradisional, mencerminkan ketekunan dan keahlian para pengrajin.
+        - **Penggunaan Modern:** Digunakan dalam busana modern seperti pakaian, tas, dan aksesori.
+        - **Pelestarian Budaya:** Penggunaan ulos dalam kehidupan sehari-hari membantu melestarikan warisan budaya Batak.
         """,
         unsafe_allow_html=True
     )
